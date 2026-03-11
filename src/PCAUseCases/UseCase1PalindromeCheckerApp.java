4c4ba1c0463607ed16c95bc24161f6d9105b8df0
vinaychauhan23 <vinaychauhan5380@gmail.com>
2026-03-11 15:00:20 +0530

    UC1: Add welcome message module

---
+++ src/PCAUseCases/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+package PCAUseCases;
+
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("     PALINDROME CHECKER APP");
+        System.out.println("==================================");
+
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Application Version : 1.0");
+
+        System.out.println("Welcome to the Palindrome Checker Application.");
+
+        System.out.println("Program Started Successfully.");
+
+        System.out.println("==================================");
+    }
+}
