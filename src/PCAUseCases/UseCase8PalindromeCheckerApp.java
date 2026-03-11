7e0ba7d773cbd9d15261209dcf5b182a3ef52b45
vinaychauhan23 <vinaychauhan5380@gmail.com>
2026-03-11 15:40:30 +0530

    UC8: Linked List based palindrome checker implemented

---
+++ src/PCAUseCases/UseCase8PalindromeCheckerApp.java
@@ -0,0 +1,84 @@
+package PCAUseCases;
+
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class UseCase8PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("     PALINDROME CHECKER APP");
+        System.out.println("==================================");
+
+        String word = "madam";
+
+        // Create linked list from string
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < word.length(); i++) {
+            Node newNode = new Node(word.charAt(i));
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        // Find middle using slow & fast pointer
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node prev = null;
+        Node current = slow;
+
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        // Compare both halves
+        Node firstHalf = head;
+        Node secondHalf = prev;
+
+        boolean isPalindrome = true;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                isPalindrome = false;
+                break;
+            }
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+
+        System.out.println("Program Ended.");
+    }
+}
